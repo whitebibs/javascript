130998b5986a1162db05adcfa576b3ecf298201e
whitebibs <115084982+whitebibs@users.noreply.github.com>
2022-11-10 22:08:15 +0100

    Add files via upload

---
+++ es-13/es-23/exercise-1.js
@@ -0,0 +1,20 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+// Print values of person using Object.keys
+
+
+console.log(Object.keys(person)); //chiavi
+console.log(Object.values(person)); //valori
+console.log(Object.entries(person))//array con 2 elementi(chiave e valore)
+
+
+/*const key= Object.keys(person);
+console.log(key);
+const val=Object.values(person);
+console.log(val);
+const entry=Object.entries(person);
+console.log(entry);
+*/
