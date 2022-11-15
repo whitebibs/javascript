1be2dcaf61abdca2da4f6ca225124b5de33a7a63
whitebibs <115084982+whitebibs@users.noreply.github.com>
2022-11-15 11:16:37 +0100

    Update exercise.js

--- es-10/es-37/exercise.js
+++ es-10/es-37/exercise.js
@@ -6,3 +6,14 @@ function printName(){
     return inner;
 }
 console.log(printName()())
+
+
+/*function printName(){
+   const helloName="Hello John";
+    function inner(){
+       console.log (helloName);
+    }
+    return inner;
+}
+printName()()*/
+
