a85df40816f99b5dfc0d1fa4b75c9993ced1984f
whitebibs <115084982+whitebibs@users.noreply.github.com>
2022-11-15 17:17:09 +0100

    Add files via upload

---
+++ es-15/exercise-1.js
@@ -0,0 +1,18 @@
+function adultFilter(persons) {
+
+}
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+const adults=persons.filter((adultFilter)=>adultFilter.age>=18)
+console.log(persons);
+console.log(adults)
