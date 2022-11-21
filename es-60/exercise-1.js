a33866cb97f8fe2bb541a6817259e90480328889
whitebibs <115084982+whitebibs@users.noreply.github.com>
2022-11-21 14:32:41 +0100

    Update exercise-1.js

--- es-60/exercise-1.js
+++ es-60/exercise-1.js
@@ -3,5 +3,5 @@ let num2 = 20;
 console.log('Before swap: ', num1, num2); // Before swap: 10 20
 
 [num1, num2]=[num2,num1];
-console.log("risultato swap:",num2, num1)
+console.log('After swap:',num2, num1)
 
