855782d544766bbc94091cba16c76fa7c47ba531
whitebibs <115084982+whitebibs@users.noreply.github.com>
2022-11-23 14:07:49 +0100

    Add files via upload

---
+++ es-17/es-55/exercise-3.js
@@ -0,0 +1,21 @@
+function calculateAverageAge(persons) {
+  const total=persons.reduce((prev,curr)=> prev+curr.age,0);
+  const average= total/persons.length;
+  return Math.round(average);
+}
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
