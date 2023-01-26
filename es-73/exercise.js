fc2ee587f63e9c1769d47efbeb219e3990cf9f97
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-01-26 14:21:36 +0100

    Update exercise.js

--- es-73/exercise.js
+++ es-73/exercise.js
@@ -1,10 +1,9 @@
 function printAsyncName(callback, name){
- setTimeout(()=>callback("hello"),1000);
- 
+ setTimeout(()=>callback(),1000);
  setTimeout(()=> console.log(name),2000)
  name="bibi";
 }
-printAsyncName(()=>console.log("Hello"))
-//non so se ho inteso bene la consegna
+printAsyncName(()=>console.log("Hello"), "Bianca")
+
 
 
